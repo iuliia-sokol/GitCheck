53f57e4b7ee7b9c227e21f711ebce16286707879
Iuliia Sokol <avare779@gmail.com>
2022-11-14 20:57:17 +0200

    Bug with octal literals fixed

--- src/js/close-result.js
+++ src/js/close-result.js
@@ -7,12 +7,12 @@ function onCloseResultClick(event) {
   refs.formWrapper.classList.remove('visually-hidden');
   refs.result.classList.add('visually-hidden');
   timer.stop();
-  refs.years.textContent = 00;
-  refs.months.textContent = 00;
-  refs.days.textContent = 00;
-  refs.hours.textContent = 00;
-  refs.minutes.textContent = 00;
-  refs.seconds.textContent = 00;
+  refs.years.textContent = '00';
+  refs.months.textContent = '00';
+  refs.days.textContent = '00';
+  refs.hours.textContent = '00';
+  refs.minutes.textContent = '00';
+  refs.seconds.textContent = '00';
   refs.nick.textContent = 'Nickname';
   refs.avatar.src = './images/favicon.ico';
   refs.name.textContent = '';
